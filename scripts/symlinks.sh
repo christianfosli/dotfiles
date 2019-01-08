1c8d302088098c782a5656f06a52cc193ef0dfab
Christian Fosli <cfosli@gmail.com>
2019-01-08 08:56:03 +0100

    Use powershell to create proper symlinks on windows

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Script creates symlinks from ~ to ~/dotfiles
+# This script works both on Unix and on Git-Bash for windows
+# As of writing this script the `ln -s` command in git-bash
+# does not create a link, so we need to have powershell do so
 
 public_dotfiles='bash_aliases bash_profile bashrc inputrc tmux.conf'
 private_dotfiles='gitconfig'
+windows() { [[ -n $WINDIR ]]; }
 
 # Back-up existing files
 printf 'Backing up current dotfiles to ~/old_dotfiles\n'
@@ -26,7 +30,13 @@ printf 'Creating symlinks for public dotfiles\n'
 
 for file in $public_dotfiles
 do
-    ln -s ~/dotfiles/$file ~/.$file
+    if windows; then
+        powershell New-Item -ItemType SymbolicLink\
+            -Path \$Home/.$file\
+            -Target \$Home/dotfiles/$file
+    else
+        ln -s ~/dotfiles/$file ~/.$file
+    fi
 done
 
 printf 'Creating symlinks from private dotfiles\n'
@@ -35,5 +45,11 @@ printf 'Creating symlinks from private dotfiles\n'
 
 for file in $private_dotfiles
 do
-    ln -s ~/dotfiles_private/$file ~/.$file
+    if windows; then
+        powershell New-Item -ItemType SymbolicLink\
+            -Path \$Home/.$file\
+            -Target \$Home/dotfiles_private/$file
+    else
+        ln -s ~/dotfiles_private/$file ~/.$file
+    fi
 done
