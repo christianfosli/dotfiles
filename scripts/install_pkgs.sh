95f3d547d089993b7eae406cd702af79338c8319
Christian Fosli <christian.fosli@capgemini.com>
2020-11-07 15:45:29 +0100

    Update install script

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
+set -eo pipefail
 
-# This includes CLI tools I use frequently
-# Tools to install manually when applicable:
-# docker, rust, texlive, etc
-
-# Set variables
 pkgs='skim neovim git curl'
-gui_pkgs='chromium gnome-tweaks'
-gui=0 # make 0 if no gui
+workstation_pkgs='chromium-freeworld gnome-tweaks docker keepassxc'
 
 if hash apt; then
     install_cmd='apt install -y'
@@ -19,19 +14,36 @@ else
     echo "ERR: could not determine package manager" && exit 1
 fi
 
-# Install non-gui apps
+# --- Install packages ---
+
 printf '\n\ninstalling packages\n\n'
 for pkg in $pkgs; do
     printf "installing $pkg\n"
-    $install_cmd $pkg \
-        || (echo "ERR: Failed on install $pkg" && exit 1)
+    $install_cmd $pkg
 done
 
-# Install gui apps
-if [[ $gui -eq 1 ]]; then
-    printf '\n\ninstalling gui apps\n\n'
-    for gui_pkg in $gui_pkgs; do
-        sudo $pkgManager install -y $gui_pkg \
-            || (echo "ERR: Failed to install $pkg" && exit 1)
-    done
-fi
+# --- Prompt for ``workstation packages'' ---
+
+printf '\n\nInstall packages for workstation?\n'
+select yn in "yes" "no"; do
+    case $yn in
+        yes)
+            for pkg in $workstation_pkgs do;
+                printf 'Installing $pkg\n'
+                $install_cmd $pkg
+            done
+            # List of things I have done in case of a clean-install
+            printf 'You need to install programming languages manually { Rust, dotnet, ... } \n'
+            printf 'You need to install tools that have their own yum repositories yourself also\n'
+            printf 'Like VSCode, Ms Teams, Kubernetes, Azure-CLI\n'
+            printf 'You need to disable SELinux to use bind mounts in docker with ease\n'
+            printf 'You need to log in with X instead of Wayland to screenshare on MS Teams\n'
+            printf 'You need to use dotnet cli to generate a self-signed PFX and set some env vars\n'
+            printf 'to run dotnet applications with https in docker\n'
+            printf 'You need to install azure artifacts cred-provider to work with private Nuget feeds\n'
+            ;;
+        no)
+            exit
+            ;;
+    esac
+done
