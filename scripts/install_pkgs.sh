c5c2f5217a7189426da493d56e9efd937651f76a
Christian Fosli <christian.fosli@capgemini.com>
2021-01-17 16:31:01 +0100

    fix typo

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -36,7 +36,7 @@ select yn in "yes" "no"; do
             printf 'You need to install programming languages manually { Rust, dotnet, ... } \n'
             printf 'You need to install tools that have their own yum repositories yourself also\n'
             printf 'Like VSCode, Ms Teams, Kubernetes, Azure-CLI\n'
-            printf 'You need to disable SELinux to use bind mounts in docker with ease\n'
+            printf 'You need to set SELinux to permissive to use bind mounts in docker with ease\n'
             printf 'You need to log in with X instead of Wayland to screenshare on MS Teams\n'
             printf 'You need to use dotnet cli to generate a self-signed PFX and set some env vars\n'
             printf 'to run dotnet applications with https in docker\n'
